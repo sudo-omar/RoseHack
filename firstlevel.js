a93d38cce7304ed5a41a6299d7057ebbfac18fa7
sudo-omar <omarabouharb2805@gmail.com>
2024-01-21 09:21:05 -0800

    change

--- firstlevel.js
+++ firstlevel.js
@@ -1,11 +1,17 @@
+var mouseX;
+var mouseY;
+var found = false;
 document.addEventListener('DOMContentLoaded', function() {
     const room = document.getElementById("room");
 
     function updateClipPath(event) {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-
-        room.style.clipPath = `circle(50px at ${mouseX}px ${mouseY}px)`;
+        if(!found) {
+            mouseX = event.clientX;
+            mouseY = event.clientY;
+            console.log(`mousex:${mouseX}, mousey:${mouseY}`)
+            room.style.clipPath = `circle(50px at ${mouseX}px ${mouseY}px)`;
+        }
+        
     }
 
     document.body.addEventListener('mousemove', updateClipPath);
@@ -17,7 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
  
     // Add click event listener to the div
     contentDiv.addEventListener("click", function () {
-       alert("Div clicked!");
+        
+       if(mouseX > 390 && mouseX < 500 
+        && mouseY > 730 && mouseY < 830) {
+            document.getElementById("room").style.clipPath = "circle(10000px at 500px 500px";
+            found = true;
+            
+            const imageroom = document.getElementById("foundmessageh");
+            const roomdiv = document.getElementById("room");
+            imageroom.innerHTML = "You found the knife";
+            setTimeout(() => { 
+                let img = document.createElement('img');
+                img.src = "33good.png";
+                img.style.zIndex = 10;
+                img.style.width = "600px";
+                img.style.height = "auto";
+                document.getElementById('body').appendChild(img);
+                document.getElementById('item').style.opacity = 0;
+                
+                
+            }, 2000);
+
+        }
     });
  });
  
